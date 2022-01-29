7456cb38f5522840a0016009bd3b1eb2092b75b8
bassel-mousa <--get>
2022-01-29 03:50:35 +0200

    Making First dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,15 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:movie_app/data/core/api_client.dart';
-import 'package:movie_app/data/data_sources/movie_remote_data_source.dart';
-import 'package:movie_app/data/repositories/movie_repository_impl.dart';
 import 'package:movie_app/domain/entities/app_error.dart';
 import 'package:movie_app/domain/entities/movie_entity.dart';
 import 'package:movie_app/domain/entities/no_params.dart';
 import 'package:movie_app/domain/usecases/get_trending.dart';
-
+import 'package:movie_app/di/get_it.dart' as getIt;
+import 'package:pedantic/pedantic.dart';
 void main() async{
+  unawaited(getIt.init());
 
-  ApiClient apiClient = ApiClient(Client());
-  MovieRemoteDataSource dataSource = MovieRemoteDataSourceImpl(apiClient);
-  MovieRepositoryImpl movieRepositoryImpl = MovieRepositoryImpl(dataSource);
-  GetTrending getTrending = GetTrending(movieRepositoryImpl);
+  GetTrending getTrending = getIt.getItInstance<GetTrending>();
   final Either<AppError, List<MovieEntity>?> either =  await getTrending(NoParams());
 
   either.fold((l) {
